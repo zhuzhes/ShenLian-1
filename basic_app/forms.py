bc6752558a3405b6798ab44d1af5e6ea3be72e2a
Zhu <michael.2.zhu@bt.com>
2019-02-27 21:50:52 +0800

    thrid

--- basic_app/forms.py
+++ basic_app/forms.py
@@ -1,13 +1,6 @@
 from django import forms
 
 class Class_Form_1(forms.Form):
-    SystemID = forms.CharField()
-    LicenseLevel = forms.CharField()
-    # uptime = forms.CharField()
-    # version = forms.CharField()
-    # free-memory = forms.CharField()
-    # total-memory = forms.CharField()
-    # cpu = forms.CharField()
-    # cpu-frequency = forms.CharField()
-    # architecture-name = forms.CharField()
-    # board-name = forms.CharField()
+    mgtIP = forms.CharField()
+    username = forms.CharField()
+    password = forms.CharField()
